1abd14b32d3a763b86d12f4b4564e29346343d34
Snack2180 <jongmoon1221@naver.com>
2024-02-02 03:54:06 +0900

    자료형
    숫자, 시퀀스, 매핑, 불, 세트

---
+++ python/DataType_09.py
@@ -0,0 +1,49 @@
+# Data Type
+
+### 자료형 - 숫자인지 문자인지 표시
+
+type()          # 자료형 확인 명령어
+
+type(6)     # 정수
+type (int)
+
+type('A')   # 문자열
+type (str)
+
+### 숫자
+
+(int)       # 정수
+1000000
+
+(float)     # 부동소수점수
+2.8654896
+
+(complex)   # 복소수
+3 + 4j      # j = 허수(i)
+
+### 시퀀스 - for 문에서 사용할 수 있는 것들
+
+(str) # 문자열
+"High Risk, High Return"
+
+(list) # 리스트
+['Moeny', 'Time']
+
+(tuple) # 튜플
+('Money', 'Time')
+
+### 매핑
+
+(dict) # 딕셔너리 - 키(key) : 값(value)
+({'Month' : 2, 'day' : 1, 'year' : 2024})
+
+### 불 - 참, 거짓
+
+type(True)
+(bool)
+type(True == 'True')
+(bool)
+
+### 세트 - 집합 표현, 집합 연산 사용 가능
+
+world = {'Money', 'Time', 'People'}
